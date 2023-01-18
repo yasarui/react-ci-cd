7f4a59e9638b8a2331a2540d1a87bf4980b47991
Yasar Sulaiman <yasar.sulaiman@supportlogic.com>
2023-01-18 14:58:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Angular
+          Learn Everything before you go
         </a>
       </header>
     </div>
